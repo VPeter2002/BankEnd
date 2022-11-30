30c90feb1271142eed96e6534a6a14a923cf247b
Vesz-Peter <Vesz-Peter@stud.uni-obuda.hu>
2022-11-30 22:32:51 +0100

    finish maybe 2.0

---
+++ AD41HN_HFT_2022231.Endpoint/Controllers/Non-crudController.cs
@@ -0,0 +1,54 @@
+﻿using AD41HN_HFT_2022231.Logic.Interfaces;
+using AD41HN_HFT_2022231.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AD41HN_HFT_2022231.Endpoint.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class Non_crudController : ControllerBase
+    {
+        IPlayerLogic playerlogic;
+        ITeamLogic teamlogic;
+        ITrainerLogic trainerlogic;
+
+        [HttpGet("{post}")]
+
+        public IEnumerable GetPlayersOnThisPost(string post)
+        {
+            return this.playerlogic.GetPlayersOnThisPost(post);
+
+        }
+        [HttpGet("{Playername}")]
+
+        public IEnumerable GetTeamName(string Playername)
+        {
+            return this.playerlogic.GetTeamName(Playername);
+        }
+        [HttpGet("{Playername}")]
+
+        public IEnumerable GetTrainerName(Player Playername)
+        {
+            return this.playerlogic.GetTrainerName(Playername);
+        }
+        [HttpGet("{teamname}")]
+
+        public IEnumerable GetGoalKeepersInTeam(string teamname)
+        {
+            return this.teamlogic.GetGoalKeepersInTeam(teamname);
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Team> GetTeamOfTrainer_id(int id)
+        {
+
+            return this.trainerlogic.GetTeamOfTrainer_id(id);
+        }
+
+
+    }
+}
